825b24c86610fddec49125362310eb0bb4cfe6f3
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-08-06 10:05:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    packages=find_packages(),
+    version='0.0.0',
+    author='Deepanshu',
+    author_email='deepeditz29@gmail.com',
+    install_requires = [],
+)
