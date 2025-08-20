72380518a6d085d476512c10234e43088808a5d0
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2025-08-20 14:53:32 +0530

    vercel

--- src/helper.py
+++ src/helper.py
@@ -1,27 +1,3 @@
-# from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter 
-# from langchain.embeddings import HuggingFaceEmbeddings
-
-# def load_pdf_file(data):
-#     loader = DirectoryLoader(data,
-#                              glob="*.pdf",
-#                              loader_cls=PyPDFLoader)
-#     documents = loader.load()
-#     return documents
-
-# def text_split(extracted_data):
-#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-#     texts_chunks = text_splitter.split_documents(extracted_data)
-#     return texts_chunks
-
-# def download_huggingface_embeddings():
-#     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-#     return embeddings
-
-
-
-
-
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter 
 from langchain_community.embeddings import HuggingFaceEmbeddings
